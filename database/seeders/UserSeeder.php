222a40cc8f68b11676e262c37eb243e3429f13b8
adoravelc <s160722034@student.ubaya.ac.id>
2025-03-29 18:19:25 +0700

    tambah userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -15,10 +16,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Insert admin user
         DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'gmail.com',
-            'password'=>Hash::make('password'),
+            'name' => 'chavel',
+            'email' => 'chavelratu@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('Syavel#20'),
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
+
+        // Insert 3 regular users
+        for ($i = 0; $i < 3; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@gmail.com',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
