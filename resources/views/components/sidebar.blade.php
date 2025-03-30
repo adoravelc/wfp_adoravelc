2995c9e66522cc0bb156c421192ec894010db15e
adoravelc <s160722034@student.ubaya.ac.id>
2025-03-30 21:37:02 +0700

    tambah logo, ganti css, sidebar perbaiki

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,16 @@
 <div class="sidebar">
-    <a href="{{ url('/laporan') }}">Reports</a>
-    <a href="{{ url('/daftar-kategori') }}">Categories</a>
-    <a href="{{ url('/daftar-makanan') }}">Foods</a>
-    <a href="{{ url('/daftar-order') }}">Orders</a>
-    <a href="{{ url('/daftar-customer') }}">Customers</a>
+    <div class="sidebar-header">
+        <!-- Logo -->
+        <a href="{{ url('/') }}" class="sidebar-logo">
+            <img src="{{ asset('asset/logo_adoravelc.png') }}" alt="Logo" class="sidebar-logo-img">
+        </a>
+    </div>
+    <!-- Sidebar Links -->
+    <a href="{{ url('/laporan') }}" class="{{ request()->is('laporan') ? 'active' : '' }}">Reports</a>
+    <a href="{{ url('/daftar-kategori') }}" class="{{ request()->is('daftar-kategori') ? 'active' : '' }}">Categories</a>
+    <a href="{{ url('/daftar-makanan') }}" class="{{ request()->is('daftar-makanan') ? 'active' : '' }}">Foods</a>
+    <a href="{{ url('/daftar-order') }}" class="{{ request()->is('daftar-order') ? 'active' : '' }}">Orders</a>
+    <a href="{{ url('/daftar-customer') }}" class="{{ request()->is('daftar-customer') ? 'active' : '' }}">Customers</a>
+
     @include('components.footer')
 </div>
